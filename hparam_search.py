dd9d022779c8072171113f41b45cb39890345c3b
kcorder <kcorder@udel.edu>
2022-03-08 11:07:58 -0500

    Fix bug with n_trials in hparam_search.py: added --n-trials CLI option for num samples.

--- hparam_search.py
+++ hparam_search.py
@@ -6,19 +6,12 @@ import os
 import torch
 from algorithms.nfsp import save_name
 import numpy as np
-from argparse import Namespace
 import random
 from experiment_train import trainer_types
 import optuna
 from optuna.trial import TrialState
 from multiprocessing import Process
 
-
-def validate_args(args: Namespace):
-    env_list = args.envs.split(',')
-    if len(env_list) > 1 and args.num_concurrent > 1:
-        raise ValueError("This hasn't been tested!")
-    return args
 
 
 parser = argparse.ArgumentParser(description="Run an multiagent Atari benchmark.")
@@ -42,6 +35,8 @@ parser.add_argument("--study-name", type=str, default=None,
 parser.add_argument("--db-name", type=str, default="maale",
                     help="name of SQL table name. Uses old name as default for testing purposes.")
 parser.add_argument("--db-password", type=str)
+parser.add_argument("--n-trials", type=int, default=100,
+                    help="number of trials for EACH environment, or how many times hparams are sampled.")
 args = parser.parse_args()
 
 
@@ -173,16 +168,21 @@ if __name__ == "__main__":
                                     study_name=args.study_name,
                                     load_if_exists=True)
 
-    procs = []
-    for env_id in env_list:
+    num_trials = 0
+    while num_trials < args.num_trials:
+        procs = []
         for i in range(args.num_concurrent):
-            objective_fn = lambda trial: objective(trial, env_id=env_id, parallel_num=i, hparams=sample_dqn_params(trial))
-            proc_target = lambda: study.optimize(objective_fn, n_trials=100//args.num_concurrent, timeout=600)
-            p = Process(target=proc_target)
-            p.start()
-            procs.append(p)
-    for p in procs:
-        p.join()
+            # for each concurrent run, sample hparams ONCE for all envs
+            num_trials += 1
+            sampled_objective = lambda trial, _env_id: objective(trial, env_id=_env_id, parallel_num=i, hparams=sample_dqn_params(trial))
+            for env_id in env_list:
+                objective_fn = lambda trial: sampled_objective(trial, env_id)
+                proc_target = lambda: study.optimize(objective_fn, n_trials=1, timeout=600)
+                p = Process(target=proc_target)
+                p.start()
+                procs.append(p)
+        for p in procs:
+            p.join()
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
