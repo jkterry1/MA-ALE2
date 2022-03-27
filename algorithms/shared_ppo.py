333da00f7be2da94704e6986fb9da58f0e3ad576
kcorder <kcorder@udel.edu>
2022-03-27 14:58:23 -0400

    Fix ppo preset function

--- algorithms/shared_ppo.py
+++ algorithms/shared_ppo.py
@@ -23,20 +23,11 @@ def make_ppo_vec(env_name, device, _, **kwargs):
     venv = make_vec_env(env_name, device=device, vs_builtin=False)
 
     hparams = kwargs.get('hparams', {})
-    preset = atari.ppo.env(venv).device(device).hyperparameters(n_envs=venv.num_envs, **hparams).build()
-    # preset = atari.ppo.env(venv).device(device).hyperparameters(
-    #     n_envs=venv.num_envs,
-    #     n_steps=32,
-    #     minibatches=8,
-    #     epochs=4,
-    #     feature_model_constructor=nat_features,
-    #     # value_model_constructor=impala_value_head,
-    #     # policy_model_constructor=impala_policy_head,
-    #     entropy_loss_scaling=0.001,
-    #     value_loss_scaling=0.1,
-    #     clip_initial=0.5,
-    #     clip_final=0.05,
-    # ).build()
+    preset = atari.ppo.env(venv).device(device).hyperparameters(
+        n_envs=venv.num_envs,
+        feature_model_constructor=nat_features,
+        **hparams
+    ).build()
 
     experiment = ParallelEnvExperiment(preset, venv)
     return experiment, preset, venv
